b489cc0324735adbf30aa20c8c497a8ac8e65a11
Vintage <vincepeo@yahoo.com>
2023-11-09 22:00:13 -0500

    routes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
 
+// import controller function for thought routes
+const { 
+  getThoughts, 
+  getSingleThought, 
+  addThought, 
+  updateThought, 
+  deleteThought, 
+  addReaction, 
+  deleteReaction } = require('../../controllers/thoughtsController');
+
+// routes for managing thoughts and reaction
+router.route('/')
+  .get(getThoughts)
+  .post(addThought);
+
+router.route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+  .post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
+
+// export router
 module.exports = router;
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,3 +1,28 @@
 const router = require('express').Router();
 
+// import controller function for user routes
+const { 
+  getUsers, 
+  getSingleUser, 
+  addUser, 
+  updateUser, 
+  deleteUser, 
+  addFriend, 
+  deleteFriend } = require('../../controllers/usersController');
+
+// define routes for users and friends
+router.route('/')
+  .get(getUsers)
+  .post(addUser);
+
+router.route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend);
+
+// export router
 module.exports = router;
